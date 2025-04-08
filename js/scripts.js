cbb26a926ad93d68bd57c6ce2ae31b347adce310
alytoncogo <cogotechnologies@gmail.com>
2025-04-08 15:52:18 -0400

    feat add: toncogo | v: beta1.0 | proto

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Scroll suave nos links do menu
+document.querySelectorAll('.menu a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
